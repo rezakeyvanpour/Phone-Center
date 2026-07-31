0b3432d82229f2624ede35db254e7b53f8d8bf30
rezakeyvanpour <rezakvp143@gmail.com>
2026-07-31 18:41:08 +0330

    Changes to project folders and creating a login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: [
-    "./public/**/*.{html,js}",
+    "./templates/**/*.{html,js}",
+    "./js/**/*.js",
   ],
   theme: {
+    colors :{
+      'bg-body': '#0D1224',
+      'body-text-color': '#ffffff',
+      'body-text-color2': '#d2d2d2',
+      'body-text-color3': 'b5b5b5',
+      'primary': '#5427b0',
+      'btn-bg-color': '#5427b0',
+      'border': '#ffffff10',
+      'box': '#192139',
+      black : colors.black,
+      white : colors.white,
+      gray : colors.gray,
+      emerlad : colors.emerlad,
+      indigo : colors.indigo,
+      yellow : colors.yellow,
+    },
     extend: {},
   },
   plugins: [],
